96a35d02ca9e4d07461dc6fa8d523aaca556ca3d
Weide1003 <112137004+Weide1003@users.noreply.github.com>
2022-10-27 20:31:54 -0500

    Add files via upload

---
+++ DemoHorse.java
@@ -0,0 +1,19 @@
+public class DemoHorse{
+public static void main(String args[]){
+Horse horse1 = new Horse();
+RaceHorse horse2 = new RaceHorse();
+horse1.setName("0ld Paint");
+horse1.setColor("brown");
+horse1.setBirthYear(2009);
+horse2.setName("Champion");
+horse2.setColor("black");
+horse2.setBirthYear(2011);
+horse2.setRaces(4);
+System.out.println(horse1.getName() + " is " +
+		horse1.getcolor() + " and was born in " +horse1.getBirthYear() + ".");
+System.out.println(horse2.getName() + "is " +
+		horse2.getcolor() + " and was born in " + horse2.getBirthYear() + ".");
+System.out.println(horse2.getName()+ " has been in " +
+		horse2.getRaceso() +" races.");
+}
+}
---
+++ RaceHorse.java
@@ -0,0 +1,9 @@
+public class RaceHorse extends Horse{
+private int races;
+public int getRaceso(){
+return races;
+}
+public void setRaces(int numRaces) {
+races = numRaces;
+}
+}
